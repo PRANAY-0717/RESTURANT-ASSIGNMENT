ae12730ee3d4df993e060505e43f3ad0713c0660
PRANAY JAISWAL <pranaypgjaiswal2018@gmail.com>
2026-05-11 23:35:10 +0530

    SET UP THE BASIC ARCHITECTURE AND REMOVED THE DEFAULT nextJS code

---
+++ resturant-manager/src/app/page.js
@@ -0,0 +1,13 @@
+"use client";
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold tracking-tight">
+        Restaurant Manager
+      </h1>
+      <p className="mt-4 text-muted-foreground">
+        STARTING THE ASSIGNMENT
+      </p>
+    </main>
+  );
+}
